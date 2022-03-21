0fb8662f8dcc6b5596fc7662327e6083953073c1
favianj <mfavianj@gmail.com>
2022-03-21 17:45:41 +0700

    first commit

---
+++ resources/views/landing.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Dongle:wght@300;400;700&family=Montserrat+Alternates:ital,wght@0,500;0,800;1,700;1,800;1,900&family=Montserrat:wght@700&family=Poppins:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <title>Landing Page</title>
+</head>
+<body>
+    <div class="header">
+        <img src="{{ url('/assets/eye.png') }}" alt="filme" id="header-logo">
+        <div class="btn-group">
+            <a href="" class="btn btn-light" id="login">Login</a>
+            <a href="" class="btn btn-dark" id="signup">Sign Up</a>
+        </div>
+    </div>
+    <div class="landing-container">
+    </div>
+    <div class="info-container">
+        <div class="item1-container">
+            <div class="item1-text">
+                <h4>Watch Movie for Free</h4>
+                <h6>Watch everything you want for Free in 1 month.</h6>
+            </div>
+            <div class="right-info"></div>
+        </div>
+    </div>
+</body>
+</html>
